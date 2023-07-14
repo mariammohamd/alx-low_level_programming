7ff321a86f025b78c8049387753beb75655c13e9
Mariaa <marimohamedahmed@gmail.com>
2023-07-14 14:08:03 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+putchar(n + '0');
+putchar(' ');
+putchar(',');
+n++;
+}
+putchar('\n');
+return (0);
+}
